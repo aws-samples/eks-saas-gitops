ec042a92be4c7bd008fa4a65c27e9f462c648af0
Humberto Somensi <somensi@amazon.com>
2023-10-14 00:08:34 +0100

    removed scripts

--- cleanup.sh
+++ cleanup.sh
@@ -17,20 +17,20 @@ catch_error() {
      curl -X PUT --data-binary "$JSON_DATA" "$EVENT_RESPONSE_URL"
 }
 
-TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 60")
-AWS_REGION=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/\(.*\)[a-z]/\1/')
-export TERRAFORM_CLUSTER_FOLDER="/home/ec2-user/environment/eks-saas-gitops/terraform/clusters/production"
-export APPLICATION_PLANE_INFRA_FOLDER="/home/ec2-user/environment/eks-saas-gitops-aws/terraform/application-plane/production/environments"
+# TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 60")
+# AWS_REGION=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/\(.*\)[a-z]/\1/')
+# export TERRAFORM_CLUSTER_FOLDER="/home/ec2-user/environment/eks-saas-gitops/terraform/clusters/production"
+# export APPLICATION_PLANE_INFRA_FOLDER="/home/ec2-user/environment/eks-saas-gitops-aws/terraform/application-plane/production/environments"
 
-cd $TERRAFORM_CLUSTER_FOLDER || exit
-terraform destroy -var "aws_region=${AWS_REGION}" --auto-approve -force
+# cd $TERRAFORM_CLUSTER_FOLDER || exit
+# terraform destroy -var "aws_region=${AWS_REGION}" --auto-approve -force
 
-cd $APPLICATION_PLANE_INFRA_FOLDER || exit 
-terraform destroy --auto-approve
+# cd $APPLICATION_PLANE_INFRA_FOLDER || exit 
+# terraform destroy --auto-approve
 
 export JSON_DATA="{
      \"Status\" : \"SUCCESS\",
-     \"Reason\" : \"install completed\",
+     \"Reason\" : \"clean up completed\",
      \"StackId\" : \"$EVENT_STACK_ID\",
      \"PhysicalResourceId\" : \"$PHYSICAL_RESOURCE_ID\",
      \"RequestId\" : \"$EVENT_REQUEST_ID\",
--- setup.sh
+++ setup.sh
@@ -15,82 +15,82 @@ catch_error() {
     curl -X PUT --data-binary "$JSON_DATA" "$EVENT_RESPONSE_URL"
 }
 
-# Set system locale
-echo 'LANG=en_US.utf-8' >> /etc/environment
-echo 'LC_ALL=en_US.UTF-8' >> /etc/environment
+# # Set system locale
+# echo 'LANG=en_US.utf-8' >> /etc/environment
+# echo 'LC_ALL=en_US.UTF-8' >> /etc/environment
 
-# Source the user's bashrc
-source /home/ec2-user/.bashrc
+# # Source the user's bashrc
+# source /home/ec2-user/.bashrc
 
-# Update system packages and install dependencies
-echo '=== UPDATE system packages and INSTALL dependencies ==='
-yum update -y
-yum install -y vim git jq bash-completion moreutils gettext yum-utils perl-Digest-SHA tree
+# # Update system packages and install dependencies
+# echo '=== UPDATE system packages and INSTALL dependencies ==='
+# yum update -y
+# yum install -y vim git jq bash-completion moreutils gettext yum-utils perl-Digest-SHA tree
 
-# Enable Amazon Extras EPEL Repository and install Git LFS
-echo '=== ENABLE Amazon Extras EPEL Repository and INSTALL Git LFS ==='
-yum install -y amazon-linux-extras
-amazon-linux-extras install epel -y
-yum install -y git-lfs
+# # Enable Amazon Extras EPEL Repository and install Git LFS
+# echo '=== ENABLE Amazon Extras EPEL Repository and INSTALL Git LFS ==='
+# yum install -y amazon-linux-extras
+# amazon-linux-extras install epel -y
+# yum install -y git-lfs
 
-# Install AWS CLI v2
-echo '=== INSTALL AWS CLI v2 ==='
-curl 'https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip' -o 'awscliv2.zip'
-unzip awscliv2.zip -d /tmp
-/tmp/aws/install --update
-rm -rf /tmp/aws awscliv2.zip
+# # Install AWS CLI v2
+# echo '=== INSTALL AWS CLI v2 ==='
+# curl 'https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip' -o 'awscliv2.zip'
+# unzip awscliv2.zip -d /tmp
+# /tmp/aws/install --update
+# rm -rf /tmp/aws awscliv2.zip
 
-# Install Kubernetes CLI (kubectl)
-echo '=== INSTALL Kubernetes CLI ==='
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod +x kubectl
-mv kubectl /usr/local/bin/
-/usr/local/bin/kubectl completion bash > /etc/bash_completion.d/kubectl
+# # Install Kubernetes CLI (kubectl)
+# echo '=== INSTALL Kubernetes CLI ==='
+# curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+# chmod +x kubectl
+# mv kubectl /usr/local/bin/
+# /usr/local/bin/kubectl completion bash > /etc/bash_completion.d/kubectl
 
-# Install Helm CLI
-echo '=== INSTALL Helm CLI ==='
-curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
-/usr/local/bin/helm completion bash > /etc/bash_completion.d/helm
+# # Install Helm CLI
+# echo '=== INSTALL Helm CLI ==='
+# curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+# /usr/local/bin/helm completion bash > /etc/bash_completion.d/helm
 
-# Install Eksctl CLI
-echo '=== INSTALL Eksctl CLI ==='
-curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
-mv /tmp/eksctl /usr/local/bin
-/usr/local/bin/eksctl completion bash > /etc/bash_completion.d/eksctl
+# # Install Eksctl CLI
+# echo '=== INSTALL Eksctl CLI ==='
+# curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+# mv /tmp/eksctl /usr/local/bin
+# /usr/local/bin/eksctl completion bash > /etc/bash_completion.d/eksctl
 
-# Install Flux CLI
-echo '=== INSTALL Flux CLI ==='
-curl --silent --location "https://github.com/fluxcd/flux2/releases/download/v0.41.2/flux_0.41.2_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
-mv /tmp/flux /usr/local/bin
-/usr/local/bin/flux completion bash > /etc/bash_completion.d/flux
+# # Install Flux CLI
+# echo '=== INSTALL Flux CLI ==='
+# curl --silent --location "https://github.com/fluxcd/flux2/releases/download/v0.41.2/flux_0.41.2_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+# mv /tmp/flux /usr/local/bin
+# /usr/local/bin/flux completion bash > /etc/bash_completion.d/flux
 
-# Install PLUTO
-echo '=== INSTALL PLUTO ==='
-curl --silent --location "https://github.com/FairwindsOps/pluto/releases/download/v5.16.1/pluto_5.16.1_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
-mv /tmp/pluto /usr/local/bin
+# # Install PLUTO
+# echo '=== INSTALL PLUTO ==='
+# curl --silent --location "https://github.com/FairwindsOps/pluto/releases/download/v5.16.1/pluto_5.16.1_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+# mv /tmp/pluto /usr/local/bin
 
-# Install KUBENT
-echo '=== INSTALL KUBENT ==='
-curl --silent --location "https://github.com/doitintl/kube-no-trouble/releases/download/0.7.0/kubent-0.7.0-$(uname -s)-amd64.tar.gz" | tar xz -C /tmp
-mv /tmp/kubent /usr/local/bin
+# # Install KUBENT
+# echo '=== INSTALL KUBENT ==='
+# curl --silent --location "https://github.com/doitintl/kube-no-trouble/releases/download/0.7.0/kubent-0.7.0-$(uname -s)-amd64.tar.gz" | tar xz -C /tmp
+# mv /tmp/kubent /usr/local/bin
 
-# Install kubectl convert plugin
-echo '=== INSTALL kubectl convert plugin ==='
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl-convert"
-install -o root -g root -m 0755 kubectl-convert /usr/local/bin/kubectl-convert
+# # Install kubectl convert plugin
+# echo '=== INSTALL kubectl convert plugin ==='
+# curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl-convert"
+# install -o root -g root -m 0755 kubectl-convert /usr/local/bin/kubectl-convert
 
-# Install Terraform CLI
-echo '=== INSTALL Terraform CLI ==='
-yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-yum -y install terraform
+# # Install Terraform CLI
+# echo '=== INSTALL Terraform CLI ==='
+# yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+# yum -y install terraform
 
-aws cloud9 update-environment --environment-id "$C9_PID" --managed-credentials-action DISABLE
+# aws cloud9 update-environment --environment-id "$C9_PID" --managed-credentials-action DISABLE
 
-# Provision Terraform
-echo '====== Provision Terraform ======'
-git clone https://github.com/somensi-aws/eks-saas-gitops.git /home/ec2-user/environment/eks-saas-gitops
-chmod +x /home/ec2-user/environment/eks-saas-gitops/install.sh
-/home/ec2-user/environment/install.sh
+# # Provision Terraform
+# echo '====== Provision Terraform ======'
+# git clone https://github.com/somensi-aws/eks-saas-gitops.git /home/ec2-user/environment/eks-saas-gitops
+# chmod +x /home/ec2-user/environment/eks-saas-gitops/install.sh
+# /home/ec2-user/environment/install.sh
 
 # Reboot the system
 #shutdown -r +1

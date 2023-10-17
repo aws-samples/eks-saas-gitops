77771dc39fd37922618df18bb64d9db29975eb32
Humberto Somensi <somensi@amazon.com>
2023-10-17 13:32:46 +0100

    sending response to CFN from install.sh

--- install.sh
+++ install.sh
@@ -11,9 +11,27 @@ TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-meta
 AWS_REGION=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/\(.*\)[a-z]/\1/')
 echo "export AWS_REGION=${AWS_REGION}" >> /root/.bashrc
 
-# read ssm parameter with CFN feedback info
-echo "echo $(aws ssm get-parameter --name \"eks-saas-gitops-custom-resource-event\")" >> /root/.bashrc
-echo "echo $(aws ssm get-parameter --name \"eks-saas-gitops-custom-resource-event\")" >> /home/ec2-user/.bashrc
+#get cfn parameter from ssm
+CFN_PARAMETER="$(aws ssm get-parameter --name "eks-saas-gitops-custom-resource-event" --query "Parameter.Value" --output text)" 
+
+#set variables
+STATUS="SUCCESS"
+EVENT_STACK_ID=$(echo "$CFN_PARAMETER" | jq -r .StackId)
+EVENT_REQUEST_ID=$(echo "$CFN_PARAMETER" | jq -r .RequestId)
+EVENT_LOGICAL_RESOURCE_ID=$(echo "$CFN_PARAMETER" | jq -r .LogicalResourceId)
+EVENT_RESPONSE_URL=$(echo "$CFN_PARAMETER" | jq -r .ResponseURL)
+
+JSON_DATA='{
+     "Status": "'"$STATUS"'",
+     "Reason": "Terraform executed successfully from Cloud9",
+     "StackId": "'"$EVENT_STACK_ID"'",
+     "PhysicalResourceId": "Terraform",
+     "RequestId": "'"$EVENT_REQUEST_ID"'",
+     "LogicalResourceId": "'"$EVENT_LOGICAL_RESOURCE_ID"'"
+}'
+
+# Send the JSON data using curl
+curl -X PUT --data-binary "$JSON_DATA" "$EVENT_RESPONSE_URL"
 exit
 
 # APPLY TERRAFORM NO FLUX

e9ecbe7aada94a28a82c1b0a4652fcbd1f7f7d7d
lusoal <lucasdu@amazon.com>
2023-10-23 13:03:14 -0500

    Added CodeBuild new base image for building

--- terraform/modules/codebuild/main.tf
+++ terraform/modules/codebuild/main.tf
@@ -47,7 +47,7 @@ resource "aws_codebuild_project" "example" {
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/docker:17.09.0"
+    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
     privileged_mode             = true

b0ed280d9e701879d2585350768b334fb9aaeeda
lusoal <lucasdu@amazon.com>
2023-10-23 13:08:38 -0500

    added consumer infra pipeline reqs

--- terraform/clusters/production/pipelines.tf
+++ terraform/clusters/production/pipelines.tf
@@ -4,6 +4,7 @@ resource "aws_s3_bucket" "codeartifacts" {
   bucket = "codestack-artifacts-bucket-${random_uuid.uuid_2.result}"
 }
 
+# Producer Pipeline
 module "codebuild_producer_project" {
   source = "../../modules/codebuild"
   vpc_id = module.vpc.vpc_id
@@ -18,5 +19,23 @@ module "codepipeline_producer" {
   pipeline_name = "producer-pipeline"
   codebuild_project = "producer-codebuild"
   repo_name = module.codecommit-producer.name
+  bucket_id = aws_s3_bucket.codeartifacts.id
+}
+
+# Consumer Pipeline
+module "codebuild_consumer_project" {
+  source = "../../modules/codebuild"
+  vpc_id = module.vpc.vpc_id
+  codebuild_project_name = "consumer-codebuild"
+  private_subnet_list = module.vpc.private_subnets
+  bucket_id = aws_s3_bucket.codeartifacts.id
+  repo_uri = aws_ecr_repository.consumer_container.repository_url # Interpolated to buildspec.yml
+}
+
+module "codepipeline_consumer" {
+  source = "../../modules/codepipeline"
+  pipeline_name = "consumer-pipeline"
+  codebuild_project = "consumer-codebuild"
+  repo_name = module.codecommit-consumer.name
   bucket_id = aws_s3_bucket.codeartifacts.id
 }

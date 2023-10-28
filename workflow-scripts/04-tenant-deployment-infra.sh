abefed4b1b73b307a006782f2b1898fd44d40ad9
Rodrigo Bersa <rodrigo.bersa@gmail.com>
2023-10-27 20:37:55 -0400

    v1 deployment scripts

--- workflow-scripts/04-tenant-deployment-infra.sh
+++ workflow-scripts/04-tenant-deployment-infra.sh
@@ -1,9 +1,8 @@
 # Set the desired values for AWS_REGION, TENANT_ID, TENANT_MODEL
-TENANT_ID="$1"
-TENANT_MODEL="$2"
-git_user_email="$3"
-git_user_name="$4"
-REPOSITORY_BRANCH="$5"
+TENANT_MODEL="$1"
+git_user_email="$2"
+git_user_name="$3"
+REPOSITORY_BRANCH="$4"
 
 # Define the filename of the Terraform script
 TENANT_TF_PATH="/mnt/vol/eks-saas-gitops/terraform/application-plane/production/environments"
@@ -13,25 +12,28 @@ TERRAFORM_SCRIPT_TEMPLATE_SILO="${TENANT_TF_TEMPLATE_PATH}/silo-template.tf.temp
 TERRAFORM_SCRIPT_TEMPLATE_HYBRID="${TENANT_TF_TEMPLATE_PATH}/hybrid-template.tf.template"
 TERRAFORM_SCRIPT_TEMPLATE_POOL="${TENANT_TF_TEMPLATE_PATH}/pool-template.tf.template"
 
-TERRAFORM_SCRIPT="${TENANT_TF_PATH}/${TENANT_ID}-${TENANT_MODEL}.tf"
-
-if [ "$TENANT_MODEL" == "hybrid" ]; then
-    cp "$TERRAFORM_SCRIPT_TEMPLATE_HYBRID" "$TERRAFORM_SCRIPT"
-elif [ "$TENANT_MODEL" == "silo" ]; then
-    cp "$TERRAFORM_SCRIPT_TEMPLATE_SILO" "$TERRAFORM_SCRIPT"
-elif [ "$TENANT_MODEL" == "pool" ]; then
-    cp "$TERRAFORM_SCRIPT_TEMPLATE_POOL" "$TERRAFORM_SCRIPT"
-fi
-
-echo "$TERRAFORM_SCRIPT"
-
-cat "$TERRAFORM_SCRIPT"
+for TENANT_FILE in $(ls $TENANT_TF_PATH/tenant*)
+  do
+    if [[ "$TENANT_FILE" == *"hybrid"* ]]; then
+      cp "$TERRAFORM_SCRIPT_TEMPLATE_HYBRID" "${TENANT_FILE}"
+    elif [[ "$TENANT_FILE" == *"silo"* ]]; then
+      cp "$TERRAFORM_SCRIPT_TEMPLATE_SILO" "${TENANT_FILE}"
+    elif [[ "$TENANT_FILE" == *"pool"* ]]; then
+      cp "$TERRAFORM_SCRIPT_TEMPLATE_POOL" "${TENANT_FILE}"
+    fi
+done
 
 # Perform sed replacements based on the platform
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    sed -i "" "s|__TENANT_ID__|$TENANT_ID|g" "$TERRAFORM_SCRIPT"
+  for TENANT_ID in $(cd $TENANT_TF_PATH; ls tenant* | cut -d- -f1,2)
+    do
+      sed -i "" "s|__TENANT_ID__|$TENANT_ID|g" $TENANT_TF_PATH/*.tf
+    done
 else
-    sed -i "s|__TENANT_ID__|$TENANT_ID|g" "$TERRAFORM_SCRIPT"
+  for TENANT_ID in $(cd $TENANT_TF_PATH; ls tenant* | cut -d- -f1,2)
+    do
+      sed -i "s|__TENANT_ID__|$TENANT_ID|g" $TENANT_TF_PATH/*.tf
+    done
 fi
 
 echo "Replacements completed successfully."

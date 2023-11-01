a3a703b6e194c77f3d4b92a37f7b468d531df2a2
tiagoReichert <tiago@reichert.eti.br>
2023-11-01 09:38:57 -0300

    fix: typo in tf outputs

--- terraform/modules/tenant-apps/outputs.tf
+++ terraform/modules/tenant-apps/outputs.tf
@@ -1,5 +1,5 @@
-output "producer_sqs_arn" {
-  value = try(aws_sqs_queue.producer_sqs[0].arn, null)
+output "consumer_sqs_arn" {
+  value = try(aws_sqs_queue.consumer_sqs[0].arn, null)
 }
 
 output "consumer_ddb_arn" {

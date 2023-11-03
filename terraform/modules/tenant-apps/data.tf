5f099d28b3dc9ed56bdd1e331c603f8a77fffb08
tiagoReichert <tiago@reichert.eti.br>
2023-11-03 13:48:38 -0300

    feat: Added parameter store per tenant

--- terraform/modules/tenant-apps/data.tf
+++ terraform/modules/tenant-apps/data.tf
@@ -1,3 +1,18 @@
 data "aws_eks_cluster" "eks-saas-gitops" {
   name = "eks-saas-gitops"
 }
+
+data "aws_ssm_parameter" "pool_1_consumer_sqs" {
+  count  = var.enable_consumer == false ? 1 : 0
+  name = "/pooled-1/consumer_sqs"
+}
+
+data "aws_ssm_parameter" "pool_1_consumer_ddb" {
+  count  = var.enable_consumer == false ? 1 : 0
+  name = "/pooled-1/consumer_ddb"
+}
+
+# data "aws_ssm_parameter" "pool_1_payments_bucket" {
+#   count  = var.enable_payments == false ? 1 : 0
+#   name = "/pooled-1/payments_bucket"
+# }

0a2953951131a20a890db209fc6349e9d4378939
Rodrigo Bersa <rodrigo.bersa@gmail.com>
2023-11-13 18:40:53 -0500

    Terraform fmt

--- terraform/modules/tenant-apps/data.tf
+++ terraform/modules/tenant-apps/data.tf
@@ -6,13 +6,13 @@ data "aws_eks_cluster" "eks-saas-gitops" {
 }
 
 data "aws_ssm_parameter" "pool_1_consumer_sqs" {
-  count  = var.enable_consumer == false ? 1 : 0
-  name = "/pooled-1/consumer_sqs"
+  count = var.enable_consumer == false ? 1 : 0
+  name  = "/pooled-1/consumer_sqs"
 }
 
 data "aws_ssm_parameter" "pool_1_consumer_ddb" {
-  count  = var.enable_consumer == false ? 1 : 0
-  name = "/pooled-1/consumer_ddb"
+  count = var.enable_consumer == false ? 1 : 0
+  name  = "/pooled-1/consumer_ddb"
 }
 
 # data "aws_ssm_parameter" "pool_1_payments_bucket" {
--- terraform/modules/tenant-apps/main.tf
+++ terraform/modules/tenant-apps/main.tf
@@ -17,8 +17,8 @@ resource "random_string" "random_suffix" {
 # ---------------------[ PRODUCER INFRASTRUCTURE ]--------------------
 # IAM POLICY FOR PRODUCER TO ACCESS CONSUMER SQS QUEUE
 resource "aws_iam_policy" "producer-iampolicy" {
-  count  = var.enable_consumer == true ? 1 : 0
-  name        = "producer-policy-${var.tenant_id}"
+  count = var.enable_consumer == true ? 1 : 0
+  name  = "producer-policy-${var.tenant_id}"
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -27,7 +27,7 @@ resource "aws_iam_policy" "producer-iampolicy" {
           "sqs:SendMessage",
           "ssm:GetParameter",
         ]
-        Effect   = "Allow"
+        Effect = "Allow"
         Resource = [
           aws_sqs_queue.consumer_sqs[0].arn,
           aws_ssm_parameter.dedicated_consumer_sqs[0].arn
@@ -39,7 +39,7 @@ resource "aws_iam_policy" "producer-iampolicy" {
 
 # IF DEDICATED PRODUCER AND CONSUMER:
 module "producer_irsa_role" {
-  count  = var.enable_producer == true && var.enable_consumer == true ? 1 : 0
+  count     = var.enable_producer == true && var.enable_consumer == true ? 1 : 0
   source    = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
   role_name = "producer-role-${var.tenant_id}"
 
@@ -57,15 +57,15 @@ module "producer_irsa_role" {
 
 # IF SHARED PRODUCER AND DEDICATED CONSUMER (HYBRID):
 resource "aws_iam_role_policy_attachment" "sto-readonly-role-policy-attach" {
-  count  = var.enable_producer == false && var.enable_consumer == true ? 1 : 0
+  count      = var.enable_producer == false && var.enable_consumer == true ? 1 : 0
   role       = "producer-role-pooled-1"
   policy_arn = aws_iam_policy.producer-iampolicy[0].arn
 }
 
 # ---------------------[ CONSUMER INFRASTRUCTURE ]--------------------
 resource "aws_iam_policy" "consumer-iampolicy" {
-  count  = var.enable_consumer == true ? 1 : 0
-  name        = "consumer-policy-${var.tenant_id}"
+  count = var.enable_consumer == true ? 1 : 0
+  name  = "consumer-policy-${var.tenant_id}"
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -82,7 +82,7 @@ resource "aws_iam_policy" "consumer-iampolicy" {
           "dynamodb:PutItem",
           "ssm:GetParameter"
         ]
-        Effect   = "Allow"
+        Effect = "Allow"
         Resource = [
           aws_dynamodb_table.consumer_ddb[0].arn,
           aws_sqs_queue.consumer_sqs[0].arn,
@@ -94,7 +94,7 @@ resource "aws_iam_policy" "consumer-iampolicy" {
   })
 }
 module "consumer_irsa_role" {
-  count  = var.enable_consumer == true ? 1 : 0
+  count     = var.enable_consumer == true ? 1 : 0
   source    = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
   role_name = "consumer-role-${var.tenant_id}"
 
@@ -110,31 +110,31 @@ module "consumer_irsa_role" {
   }
 }
 resource "aws_sqs_queue" "consumer_sqs" {
-  count  = var.enable_consumer == true ? 1 : 0
-  name = "consumer-${var.tenant_id}-${random_string.random_suffix.result}"
+  count = var.enable_consumer == true ? 1 : 0
+  name  = "consumer-${var.tenant_id}-${random_string.random_suffix.result}"
 
   tags = {
     Name = var.tenant_id
   }
 }
 resource "aws_ssm_parameter" "dedicated_consumer_sqs" {
-  count  = var.enable_consumer == true ? 1 : 0
+  count = var.enable_consumer == true ? 1 : 0
   name  = "/${var.tenant_id}/consumer_sqs"
   type  = "String"
   value = aws_sqs_queue.consumer_sqs[0].arn
 }
 resource "aws_ssm_parameter" "shared_consumer_sqs" {
-  count  = var.enable_consumer == false ? 1 : 0
+  count = var.enable_consumer == false ? 1 : 0
   name  = "/${var.tenant_id}/consumer_sqs"
   type  = "String"
   value = data.aws_ssm_parameter.pool_1_consumer_sqs[0].value
 }
 
 resource "aws_dynamodb_table" "consumer_ddb" {
-  count  = var.enable_consumer == true ? 1 : 0
-  name = "consumer-${var.tenant_id}-${random_string.random_suffix.result}"
-  hash_key = "tenant_id"
-  range_key = "message_id"
+  count        = var.enable_consumer == true ? 1 : 0
+  name         = "consumer-${var.tenant_id}-${random_string.random_suffix.result}"
+  hash_key     = "tenant_id"
+  range_key    = "message_id"
   billing_mode = "PAY_PER_REQUEST"
 
   attribute {
@@ -152,13 +152,13 @@ resource "aws_dynamodb_table" "consumer_ddb" {
   }
 }
 resource "aws_ssm_parameter" "dedicated_consumer_ddb" {
-  count  = var.enable_consumer == true ? 1 : 0
+  count = var.enable_consumer == true ? 1 : 0
   name  = "/${var.tenant_id}/consumer_ddb"
   type  = "String"
   value = aws_dynamodb_table.consumer_ddb[0].arn
 }
 resource "aws_ssm_parameter" "shared_consumer_ddb" {
-  count  = var.enable_consumer == false ? 1 : 0
+  count = var.enable_consumer == false ? 1 : 0
   name  = "/${var.tenant_id}/consumer_ddb"
   type  = "String"
   value = data.aws_ssm_parameter.pool_1_consumer_ddb[0].value

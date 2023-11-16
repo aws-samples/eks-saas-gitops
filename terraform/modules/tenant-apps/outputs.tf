3d1182d543868942a18252d712e2f8bb81bd14fa
Rodrigo Bersa <rodrigo.bersa@gmail.com>
2023-11-15 19:17:54 -0500

    Terraform fmt

--- terraform/modules/tenant-apps/outputs.tf
+++ terraform/modules/tenant-apps/outputs.tf
@@ -1,15 +1,15 @@
 output "producer" {
   description = "Outputs for Producer microservice"
   value = {
-            "irsa_role": try(module.producer_irsa_role[0].iam_role_arn, null)
-          }
+    "irsa_role" : try(module.producer_irsa_role[0].iam_role_arn, null)
+  }
 }
 
 output "consumer" {
   description = "Outputs for Consumer microservice"
   value = {
-            "irsa_role": try(module.consumer_irsa_role[0].iam_role_arn, null)
-          }
+    "irsa_role" : try(module.consumer_irsa_role[0].iam_role_arn, null)
+  }
 }
 
 # output "payments" {

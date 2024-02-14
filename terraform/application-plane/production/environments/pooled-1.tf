b7edf8b7998777ad09ef9f8abaa723ef5cdc46e1
lusoal <lucasdu@amazon.com>
2024-02-14 13:55:32 -0600

    Adjusted Helm Chart to add Terraform resource within the Helm Chart and make ingress routing easier

--- terraform/application-plane/production/environments/pooled-1.tf
+++
@@ -1,9 +0,0 @@
-# This will already come deployed along with the cluster infraestructure
-module "pooled-1" {
-  source    = "git::__MODULE_SOURCE__//terraform//modules//tenant-apps?ref=v0.0.1"
-  tenant_id = "pooled-1"
-}
-
-output "pooled-1" {
-  value = module.pooled-1
-}

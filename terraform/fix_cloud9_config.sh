4ed320fa89fab3c85207c9bdb5d2da442b1f32e2
lusoal <lucasdu@amazon.com>
2024-02-28 15:24:43 -0600

    Removed pooled-env and added a step to fix cloud9

---
+++ terraform/fix_cloud9_config.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Export EKS config to kubeconfig file
+aws eks --region "$AWS_REGION" update-kubeconfig --name eks-saas-gitops
+mkdir /home/ec2-user/.kube && cp /root/.kube/config /home/ec2-user/.kube/ && chown -R ec2-user:ec2-user /home/ec2-user/.kube/config
+
+# Removing Helm files that are not reconciled
+helm uninstall metrics-server -nkube-system --kubeconfig /root/.kube/config
+helm uninstall kubecost -nkubecost --kubeconfig /root/.kube/config
+helm uninstall karpenter -nkarpenter --kubeconfig /root/.kube/config
+
+# Reconciling again
+flux reconcile helmrelease metrics-server -nflux-system --kubeconfig /root/.kube/config
+flux reconcile helmrelease kubecost -nflux-system --kubeconfig /root/.kube/config
+flux reconcile helmrelease karpenter -nflux-system --kubeconfig /root/.kube/config
+
+# Get cfn parameter from ssm created by Lambda function
+CFN_PARAMETER="$(aws ssm get-parameter --name "eks-saas-gitops-custom-resource-event" --query "Parameter.Value" --output text)" 
+
+#set variables
+STATUS="SUCCESS"
+EVENT_STACK_ID=$(echo "$CFN_PARAMETER" | jq -r .StackId)
+EVENT_REQUEST_ID=$(echo "$CFN_PARAMETER" | jq -r .RequestId)
+EVENT_LOGICAL_RESOURCE_ID=$(echo "$CFN_PARAMETER" | jq -r .LogicalResourceId)
+EVENT_RESPONSE_URL=$(echo "$CFN_PARAMETER" | jq -r .ResponseURL)
+
+JSON_DATA='{
+     "Status": "'"$STATUS"'",
+     "Reason": "Terraform executed successfully from Cloud9",
+     "StackId": "'"$EVENT_STACK_ID"'",
+     "PhysicalResourceId": "Terraform",
+     "RequestId": "'"$EVENT_REQUEST_ID"'",
+     "LogicalResourceId": "'"$EVENT_LOGICAL_RESOURCE_ID"'"
+}'
+
+# Send the JSON data using curl
+curl -X PUT --data-binary "$JSON_DATA" "$EVENT_RESPONSE_URL"

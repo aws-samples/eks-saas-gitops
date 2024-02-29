3ce7e5d145b3a9828caaa080a1f6749d23fc404a
lusoal <lucasdu@amazon.com>
2024-02-29 13:50:18 -0600

    Added configure kubectl

--- terraform/workshop/outputs.tf
+++ terraform/workshop/outputs.tf
@@ -96,3 +96,8 @@ output "argo_events_irsa" {
 output "account_id" {
   value = data.aws_caller_identity.current.account_id
 }
+
+output "configure_kubectl" {
+  description = "Configure kubectl: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
+  value       = "aws eks --region ${data.aws_region.current.name} update-kubeconfig --name ${module.eks.cluster_name}"
+}

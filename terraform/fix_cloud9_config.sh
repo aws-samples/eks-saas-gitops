d5e3b945ae7619d6f587ab49415922935bf49189
lusoal <lucasdu@amazon.com>
2024-02-29 12:25:48 -0600

    Finished environment setup

--- terraform/fix_cloud9_config.sh
+++ terraform/fix_cloud9_config.sh
@@ -6,15 +6,15 @@ AWS_REGION=$(curl -H "X-aws-ec2-metadata-token:${TOKEN}" -s http://169.254.169.2
 aws eks --region "$AWS_REGION" update-kubeconfig --name eks-saas-gitops
 mkdir /home/ec2-user/.kube && cp /root/.kube/config /home/ec2-user/.kube/ && chown -R ec2-user:ec2-user /home/ec2-user/.kube/config
 
+sleep 100
+echo "Waiting for flux"
 # Removing Helm files that are not reconciled
-helm uninstall metrics-server -nkube-system --kubeconfig /root/.kube/config
-helm uninstall kubecost -nkubecost --kubeconfig /root/.kube/config
-helm uninstall karpenter -nkarpenter --kubeconfig /root/.kube/config
+flux delete helmrelease metrics-server -nflux-system --kubeconfig /root/.kube/config
+flux delete helmrelease kubecost -nflux-system --kubeconfig /root/.kube/config
+flux delete helmrelease karpenter -nflux-system --kubeconfig /root/.kube/config
 
 # Reconciling again
-flux reconcile helmrelease metrics-server -nflux-system --kubeconfig /root/.kube/config
-flux reconcile helmrelease kubecost -nflux-system --kubeconfig /root/.kube/config
-flux reconcile helmrelease karpenter -nflux-system --kubeconfig /root/.kube/config
+flux reconcile source git flux-system --kubeconfig /root/.kube/config
 
 # Get cfn parameter from ssm created by Lambda function
 CFN_PARAMETER="$(aws ssm get-parameter --name "eks-saas-gitops-custom-resource-event" --query "Parameter.Value" --output text)" 

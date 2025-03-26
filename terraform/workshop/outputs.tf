c561c8873bea7693eedfdd07155c60148776c45e
ande28em <emanderson05@gmail.com>
2025-03-26 11:28:52 -0500

    Added explicit VPC declaration to CFN

--- terraform/workshop/outputs.tf
+++ terraform/workshop/outputs.tf
@@ -129,3 +129,8 @@ output "configure_kubectl" {
   description = "Configure kubectl: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
   value       = "aws eks --region ${data.aws_region.current.name} update-kubeconfig --name ${module.eks.cluster_name}"
 }
+
+output "gitea_url" {
+  description = "URL to access Gitea"
+  value       = "http://${module.gitea.public_ip}:3000"
+}

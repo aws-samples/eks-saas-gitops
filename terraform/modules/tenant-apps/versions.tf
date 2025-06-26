ced182c086df1b1f3bb3123893e5081990bffc99
tiagoReichert <tiago@reichert.eti.br>
2025-06-26 10:53:04 -0300

    Fixed module versions

--- terraform/modules/tenant-apps/versions.tf
+++ terraform/modules/tenant-apps/versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0"
+      version = "~> 5.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 2.0"
+      version = "~> 2.0"
     }
   }
 }
